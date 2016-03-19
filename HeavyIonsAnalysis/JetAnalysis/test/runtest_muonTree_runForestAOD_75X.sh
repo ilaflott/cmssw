76963a8e59a472e1d508b4d95d2cf6d9f51b751b
ian laflotte <ilaflott@gmail.com>
2016-03-19 15:55:21 -0400

    empty commit message

--- HeavyIonsAnalysis/JetAnalysis/test/runtest_muonTree_runForestAOD_75X.sh
+++ HeavyIonsAnalysis/JetAnalysis/test/runtest_muonTree_runForestAOD_75X.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-##CMSSW_7_5_8, 100 events each cmsRun script
+##CMSSW_7_5_8, cmsRun test, 100 events for pp MC/DATA and 1000 events for PbPb DATA
 ##test muonTree plugin in forest scripts, where it will potentially be used
 
 cmsRun runForestAOD_PbPb_DATA_75X.py  >  testMuonTree_AOD_PbPb_DATA_75X.log     
-#cmsRun runForestAOD_pp_MC_75X.py      >  testMuonTree_AOD_pp_MC_75X.log
-#cmsRun runForestAOD_pp_DATA_75X.py    >  testMuonTree_AOD_pp_DATA_75X.log
+cmsRun runForestAOD_pp_MC_75X.py      >  testMuonTree_AOD_pp_MC_75X.log
+cmsRun runForestAOD_pp_DATA_75X.py    >  testMuonTree_AOD_pp_DATA_75X.log
 
 ##no good test file @ RU T3, leaving out for now
 #cmsRun runForestAOD_PbPb_MB_75X.py      testMuonTree_AOD_PbPb_MB_75X.log
