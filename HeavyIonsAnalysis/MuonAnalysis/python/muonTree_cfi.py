7b50a7bb26abc98335fc826981fe5c561da612c1
ian laflotte <ilaflott@gmail.com>
2016-04-18 12:12:24 -0400

    changed default cut values; minPt -> 1, minAbsEta -> 0, maxAbsEta -> 100

--- HeavyIonsAnalysis/MuonAnalysis/python/muonTree_cfi.py
+++ HeavyIonsAnalysis/MuonAnalysis/python/muonTree_cfi.py
@@ -4,7 +4,7 @@ muonTree = cms.EDAnalyzer(
     "muonTree",
     doGenParticles     = cms.bool(False),
     runOnParticleGun   = cms.bool(False),
-    minPt     = cms.double(0.0),
+    minPt     = cms.double(1.0),
     minAbsEta = cms.double(0.0),
     maxAbsEta = cms.double(100.0),
     pileupCollection   = cms.InputTag("addPileupInfo"),
